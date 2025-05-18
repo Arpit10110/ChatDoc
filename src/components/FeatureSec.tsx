c9fb933df29f910ac2f2f485c0ab4db570d8b4a8
Arpit <omagrahari55@gmail.com>
2025-05-18 17:52:51 +0530

    added the animation to it

--- src/components/FeatureSec.tsx
+++ src/components/FeatureSec.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 import SearchIcon from '@mui/icons-material/Search';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
@@ -7,7 +8,8 @@ import Image from 'next/image';
 import Icon1 from "@/assets/searchIcon.png"
 import Icon2 from "@/assets/brain.png"
 import Icon3 from "@/assets/rocket.png"
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 const FeatureSec = () => {
 
     const features = [
@@ -28,14 +30,16 @@ const FeatureSec = () => {
         }
       ];
       
-      
+        useEffect(() => {
+          AOS.init();
+        }, [])
       
       
 
   return (
     <>
         <div className='py-[8rem] bg-[#ffffff0d] ' >
-            <p className='text-center w-[60%] text-[2.5rem] m-auto max-tablet:w-[90%]  text-gray-300  font-semibold' >
+            <p data-aos="fade-down" data-aos-duration="1500" data-aos-easing="ease-in-out" data-aos-delay="100"  className='text-center w-[60%] text-[2.5rem] m-auto max-tablet:w-[90%]  text-gray-300  font-semibold' >
             Say goodbye to doc overload. ChatDoc gives you fast, smart, and simple answers—straight from your files.
             </p>
             <div>
@@ -43,6 +47,7 @@ const FeatureSec = () => {
                     {
                         features.map((i, index) => (
                            <div
+                           data-aos="flip-left" data-aos-duration="1500" data-aos-easing="ease-in-out" data-aos-delay={`${index* 100}` }
                            style={{ boxShadow: '0px 0px 20px 6px rgb(72,64,196,0.30)' }} 
                             key={index} className='w-[25%] bg-black py-[2rem] px-[1rem] rounded-[1rem] flex items-center flex-col gap-[0.5rem] hover:scale-[1.03] transition-all max-tablet:w-[40%] max-mobile:w-[70%] max-smobile:w-[80%] '  > 
                                 <Image className='w-[20%]' src={i.icon} alt='Icon'  />
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,16 +1,22 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
 import { TypeAnimation } from 'react-type-animation';
 import Image from 'next/image';
 import ImageSec1 from "@/assets/Sec1Img.png"
 import Marquee from "react-fast-marquee";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 const HeroSection = () => {
+  useEffect(() => {
+    AOS.init();
+  }, [])
+  
   return (
     <>
       <div className='w-full justify-center flex items-center min-h-[70vh] flex-col my-[3rem]   ' >
          <div className='w-[60%] flex flex-col items-center gap-[1rem] max-laptop:w-[75%] max-slaptop:w-[90%] max-mobile:w-full max-tablet:mt-[3rem] ' >
-          <div className='!text-[1.5rem] font-semibold w-[55%] max-mobile:w-[80%] flex justify-center items-center rounded-[0.5rem] ' >
+          <div data-aos="zoom-in-up" data-aos-duration="1500" data-aos-easing="ease-in-out" data-aos-delay="500"  className='!text-[1.5rem] font-semibold w-[55%] max-mobile:w-[80%] flex justify-center items-center rounded-[0.5rem] ' >
           <TypeAnimation
                         sequence={[
                           'Chat with your PDFs 🤖📄',
@@ -29,9 +35,9 @@ const HeroSection = () => {
                         repeat={Infinity}
                       />
           </div>
-           <h1 className='sans-font text-[6.5rem] text-center font-[600] max-tablet:text-[6rem] max-mobile:text-[5rem] max-smobile:text-[4.5rem] max-xsmobile:text-[4rem] max-[340px]:text-[3.5rem] ' >Turn PDFs Into Chat No Boring Stuff</h1>
-           <h6 className='text-[1.7rem] font-semibold text-gray-300 w-[60%] text-center max-mobile:w-[80%] ' >ChatDoc turns boring documents into real-time convos—get answers fast, no scrolling needed.</h6>
-           <div className='flex items-center justify-center gap-[2rem]  mt-[2rem] ' >
+           <h1 data-aos="fade-up"  data-aos-duration="1000" data-aos-easing="ease-in-out"  className='sans-font text-[6.5rem] text-center font-[600] max-tablet:text-[6rem] max-mobile:text-[5rem] max-smobile:text-[4.5rem] max-xsmobile:text-[4rem] max-[340px]:text-[3.5rem] ' >Turn PDFs Into Chat No Boring Stuff</h1>
+           <h6 data-aos="fade-up"  data-aos-duration="1500" data-aos-easing="ease-in-out" data-aos-delay="200" className='text-[1.7rem] font-semibold text-gray-300 w-[60%] text-center max-mobile:w-[80%] ' >ChatDoc turns boring documents into real-time convos—get answers fast, no scrolling needed.</h6>
+           <div data-aos="fade-up"  data-aos-duration="2000" data-aos-easing="ease-in-out" data-aos-delay="300" className='flex items-center justify-center gap-[2rem]  mt-[2rem] ' >
              <Link href={"/playground"} className=' bg-[#ffffff3b] text-white font-semibold px-[2rem] py-[0.5rem] rounded-[0.5rem] text-[1.5rem] hover:scale-[1.03] transition-all '  >Launch Playground</Link>
              <Link href={"/howitwork"} className=' bg-[#ffffff3b] text-white font-semibold px-[2rem] py-[0.5rem] rounded-[0.5rem] text-[1.5rem]  hover:scale-[1.03] transition-all' >How It Works?</Link>
            </div>
